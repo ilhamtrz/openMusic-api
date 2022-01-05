8f25a48dd72767b6c9d349201ec9786992105343
ilhamtrz <ilhamtriza@gmail.com>
2022-01-06 06:48:35 +0700

    add authentication table and fix postHandler

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -4,6 +4,8 @@ class UsersHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
   }
 
   async postUserHandler(request, h) {
--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// Albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
+
+// Songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+// Users
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -35,6 +46,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
