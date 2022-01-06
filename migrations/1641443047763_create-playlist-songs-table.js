f6e5d5499e051e94356e98acc109162cfb8538b8
ilhamtrz <ilhamtriza@gmail.com>
2022-01-06 18:25:21 +0700

    fix playlist_songs table name

--- migrations/1641443047763_create-playlist-songs-table.js
+++ migrations/1641443047763_create-playlist-songs-table.js
@@ -4,7 +4,7 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   // membuat table playlist_songs
-  pgm.createTable('playlists_songs', {
+  pgm.createTable('playlist_songs', {
     id: {
       type: 'VARCHAR(50)',
       primaryKey: true,
@@ -23,17 +23,17 @@ exports.up = (pgm) => {
     Menambahkan constraint UNIQUE, kombinasi dari kolom playlist_id dan song_id.
     Guna menghindari duplikasi data antara nilai keduanya.
   */
-  pgm.addConstraint('playlists_songs', 'unique_playlist_id_and_note_id', 'UNIQUE(playlist_id, song_id)');
+  pgm.addConstraint('playlist_songs', 'unique_playlist_id_and_note_id', 'UNIQUE(playlist_id, song_id)');
 
   /*
     Memberikan constraint foreign key pada kolom playlist_id dan song_id
     terhadap playlist.id dan song.id
   */
-  pgm.addConstraint('playlists_songs', 'fk_playlists_songs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
-  pgm.addConstraint('playlists_songs', 'fk_playlists_songs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
   // menghapus tabel playlists_songs
-  pgm.dropTable('playlists_songs');
+  pgm.dropTable('playlist_songs');
 };
