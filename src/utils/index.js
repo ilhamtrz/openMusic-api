32ffa12326c08e159e4ea9ac635a4487153d03a6
ilhamtrz <ilhamtriza@gmail.com>
2022-01-27 08:22:31 +0700

    fix album object property

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,12 +5,14 @@ const albumMapDBToModel = ({
   year,
   created_at,
   updated_at,
+  cover,
 }) => ({
   id,
   name,
   year,
   createdAt: created_at,
   updatedAt: updated_at,
+  coverUrl: cover,
 });
 
 const songMapDBToModel = ({
